38f81bdb99bcdbfb0f557e36fb7d9e19f822b963
Dan Shick <datn@users.noreply.github.com>
2020-10-13 16:03:27 +0200

    cleaned up the UI text a bit (#1)
    * cleaned up the UI text a bit
    
    * replaced matching quote mark

--- Commands.js
+++ Commands.js
@@ -1,17 +1,17 @@
 // Each method here maps to a command that you can use within Mattermost
 
 function cmdHelp() {
-  sendToMattermost("Current commands: !help !list !register !coffee !cleanup" )
+  sendToMattermost("Available commands: !help !list !register !coffee !cleanup" )
 }
 
 function cmdRegister( mmUser, gEmail ) {
   registerMmUser(mmUser, gEmail)
-  sendToMattermost(mmUser + " are registered with the email " + gEmail + "!")
+  sendToMattermost(mmUser + " has been registered with the email " + gEmail + ".")
 }
 
 function cmdCoffee( mmUser, requiredTimeSlot ) {
   if(!isMmUserRegistered(mmUser)) {
-    sendToMattermost("You (" + mmUser + ') are not registered')
+    sendToMattermost("You (" + mmUser + ') are not registered.')
     return;
   }
   
@@ -21,46 +21,46 @@ function cmdCoffee( mmUser, requiredTimeSlot ) {
   
   // TODO Implement doing coffee
   storeCoffeeRequest(mmUser, from, to)
-  sendToMattermost("You (" + mmUser + ') are registered and requesting coffee between ' + from + " and " + to + " (Berlin time)")
+  sendToMattermost("You (" + mmUser + ') are registered and requesting coffee between ' + from + " and " + to + " (Berlin time).")
 }
 
 function cmdSoon( mmUser, otherUser ) {
   if(!isMmUserRegistered(mmUser)) {
-    sendToMattermost("You (" + mmUser + ') are not registered')
+    sendToMattermost("You (" + mmUser + ') are not registered.')
     return;
   }
   if(!isMmUserRegistered(otherUser)) {
-    sendToMattermost("The user you want to coffee with (" + otherUser + ') is not registered')
+    sendToMattermost("The user you're requesting coffee with (" + otherUser + ') is not registered.')
     return;
   }
   var time = findSoonestFreeTime(getEmailFromMmUser(mmUser), getEmailFromMmUser(otherUser))
   if (!time) {
-    sendToMattermost(mmUser + ", you have no overlapping free time with" + otherUser + "in the next two hours")
+    sendToMattermost(mmUser + ", you have no overlapping free time with " + otherUser + " in the next two hours")
   } else {
     var event = createCalEvent(getEmailFromMmUser(mmUser), getEmailFromMmUser(otherUser), time.start, time.end)
   }
-  sendToMattermost(mmUser + ", you have created a coffee call for 30 mins with " + otherUser + "starting at "+ time.start)
+  sendToMattermost(mmUser + ", you have created a 30-minute coffee call with " + otherUser + "starting at "+ time.start)
 }
 
 function cmdNow( mmUser, otherUser ) {
   if(!isMmUserRegistered(mmUser)) {
-    sendToMattermost("You (" + mmUser + ') are not registered')
+    sendToMattermost("You (" + mmUser + ') are not registered.')
     return;
   }
   if(!isMmUserRegistered(otherUser)) {
-    sendToMattermost("The user you want to coffee with (" + otherUser + ') is not registered')
+    sendToMattermost("The user you're requesting coffee with (" + otherUser + ') is not registered.')
     return;
   }
   
   var event = createCalEventNow(getEmailFromMmUser(mmUser), getEmailFromMmUser(otherUser))
   
-  sendToMattermost(mmUser + ", you have created a coffee call for 30 mins with " + otherUser + " ( TBA link to call)")
+  sendToMattermost(mmUser + ", you have created a 30-minute coffee call with " + otherUser + " ( TBA link to call)")
   //sendToMattermost(mmUser + ", you have created a coffee call for 30 mins with " + otherUser + " (" + event.getLocation() + ")")
 }
 
 function cmdCleanup(){
   var deleted = deleteNonTodayRequests()
-  sendToMattermost("Deleted " + deleted + " old requests" )
+  sendToMattermost("Deleted " + deleted + " old requests." )
 }
 
 function cmdList(what) {
@@ -69,12 +69,12 @@ function cmdList(what) {
         sendToMattermost("The following users are registered:\n" + getRegisteredUserEmails().join("\n") )
         break;
       case 'today':
-        sendToMattermost("The coffee requests that currently exist for today:\n" + getCoffeeRequestsForToday().join("\n") )
+        sendToMattermost("Today's active coffee requests:\n" + getCoffeeRequestsForToday().join("\n") )
         break;
       case 'requests':
-        sendToMattermost("All the coffee requests that currently exist:\n" + getCoffeeRequests().join("\n") )
+        sendToMattermost("All existing coffee requests:\n" + getCoffeeRequests().join("\n") )
         break;
       default:
-        sendToMattermost('Unknown thing to list! Use either users, today, or requests.')
+        sendToMattermost('Don\'t know how to list that! Valid arguments: users, today, requests.')
   }
 }
